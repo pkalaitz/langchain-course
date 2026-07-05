9fb7fc370f977bf2ef3a53d335d6c655c0201b80
pkala <panagiotis959@gmail.com>
2026-07-05 18:45:41 +0300

    search-agent

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
+
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+
+@tool
+def search(query: str)-> str:
+    """
+        Tool that searches over internet
+        Args:
+            query (str): The query to search for
+        Returns:
+            The search result
+        """
+    print("Searching over internet for {query}".format(query=query))
+    print(f"Searching for {query}")
+    return "Tokyo weather is sunny"
+
+llm = ChatOpenAI()
+tools = [search]
+agent = create_agent(model=llm, tools=tools)
+
+
+
 def main():
-    print("Hello from langchain-course!")
+    print("Hello from langchain-course")
+    result = agent.invoke({"messages": HumanMessage(content="What is the weather in Tokyo?")})
+    print(result)
+
 
 
 if __name__ == "__main__":
